6c7060263c846388b2fd7f7bc6d9db04dd1efcd7
Mochamad NurFaizi <mfaiz1365@gmail.com>
2024-01-16 13:28:23 +0700

    other things

--- src/routers/user.route.js
+++
@@ -1,9 +0,0 @@
-const router = require('express').Router()
-const { createUser, loginUser, changePassword } = require('../controllers/user.controllers');
-const { authentication } = require('../middlewares/auth');
-
-router.post('/register', createUser);
-router.post('/login', loginUser);
-router.put('/update_password', authentication, changePassword );
-
-module.exports = router
